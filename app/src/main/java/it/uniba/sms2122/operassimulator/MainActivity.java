d0d0c188041b1cd5e008114488f7d9ad2644420c
CatignanoFrancesco <catignanof@gmail.com>
2022-05-24 13:21:09 +0200

    Gestito il comportamento dell'activity in onStop

--- app/src/main/java/it/uniba/sms2122/operassimulator/MainActivity.java
+++ app/src/main/java/it/uniba/sms2122/operassimulator/MainActivity.java
@@ -177,8 +177,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onDestroy() {
+        super.onDestroy();
         OperaAdvertiserService.stopAllServices(this);
     }
 
